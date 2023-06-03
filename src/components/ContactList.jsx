ff692dd75b12537c719f7d33c895fa87620dc3df
mmd <mmdsalehh@gmail.com>
2023-06-03 18:48:45 +0330

    Add: option to search in the contacts list

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 
 const ContactList = () => {
   const [contacts, setContacts] = useState(useLoaderData());
+  const [searchValue, setSearchValue] = useState("");
 
   const handleContactDelete = async (id) => {
     await ContactService.deleteContact(id);
@@ -22,31 +23,47 @@ const ContactList = () => {
 
   return (
     <div className="mt-4 space-y-2">
-      {contacts.map((contact) => (
-        <div
-          className="flex justify-between border-t last:border-b"
-          key={contact.id}
-        >
-          <div className="flex items-center">
-            <BiUserCircle className="text-4xl" />
-            <Link to={`/contact/${contact.id}`}>
-              <div className="ml-2">
-                <p className="selection:bg-white">{contact.name}</p>
-                <p className="selection:bg-white">{contact.email}</p>
-              </div>
-            </Link>
-          </div>
-          <div className="flex items-center space-x-3">
-            <Link to={`/edit/${contact.id}`}>
-              <BiEdit className="text-xl text-orange-500 hover:cursor-pointer hover:text-orange-600 active:scale-90" />
-            </Link>
-            <BiTrash
-              className="text-xl text-red-500 hover:cursor-pointer active:scale-90"
-              onClick={() => handleContactDelete(contact.id)}
-            />
-          </div>
+      {contacts.length >= 2 && (
+        <div>
+          <input
+            className="w-full rounded border p-1 focus:border-green-600 focus:outline-none"
+            placeholder="Type to search"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
         </div>
-      ))}
+      )}
+      {contacts
+        .filter(
+          (contact) =>
+            contact.name.includes(searchValue) ||
+            contact.email.includes(searchValue)
+        )
+        .map((contact) => (
+          <div
+            className="flex justify-between border-t last:border-b"
+            key={contact.id}
+          >
+            <div className="flex items-center">
+              <BiUserCircle className="text-4xl" />
+              <Link to={`/contact/${contact.id}`}>
+                <div className="ml-2">
+                  <p className="selection:bg-white">{contact.name}</p>
+                  <p className="selection:bg-white">{contact.email}</p>
+                </div>
+              </Link>
+            </div>
+            <div className="flex items-center space-x-3">
+              <Link to={`/edit/${contact.id}`}>
+                <BiEdit className="text-xl text-orange-500 hover:cursor-pointer hover:text-orange-600 active:scale-90" />
+              </Link>
+              <BiTrash
+                className="text-xl text-red-500 hover:cursor-pointer active:scale-90"
+                onClick={() => handleContactDelete(contact.id)}
+              />
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
